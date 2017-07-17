6507b185a76c7635630dc57c0cd7a33a31b99d37
artpylon <matthewgray88@gmail.com>
2017-07-17 14:24:04 -0400

    Generate index route, update my-application html ember generate route index Update my-application component html with game title and prompt to sign in to start a new game.

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model () {
-    return this.get('store').findAll('user');
+    const users = this.get('store').findAll('user');
   },
 });
+
+// const users = this.get('store').findAll('user');
+// const cards = this.get('store').findAll('card');
+// return {users: users, cards: cards}
