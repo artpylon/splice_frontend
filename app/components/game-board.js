4255ba3865d73918353f6506633ccaf5f31e6893
artpylon <matthewgray88@gmail.com>
2017-07-17 22:07:54 -0400

    Add game-board component, added function for shuffled gameboard Ran ember generate component game-board Moved game template contents to game-board template Called game-board component in game template Created deck property with shuffled cards Created gameArray property with first 15 cards Created removeGameArray function to remove cards from deck

---
+++ app/components/game-board.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  deck: Ember.computed('model.cards.[]', function () {
+    let array = this.get('model.cards.[]').toArray()
+      for (var i = array.length - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          var temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+      }
+      return array;
+  }),
+  gameArray: Ember.computed('this.deck', function () {
+      return this.get('deck').slice(0, 15)
+  }),
+  removeGameArray: Ember.computed('this.deck', function () {
+      return this.get('deck').removeObjects(0, 15)
+  }),
+});
