69230727d4be52167b02be27be660bac93fb5b4e
artpylon <matthewgray88@gmail.com>
2017-07-18 19:19:40 -0400

    Updated delete function Current status, deletes files successfully to the api, but error is returned. Next step to investigate that.

--- app/routes/game.js
+++ app/routes/game.js
@@ -7,12 +7,27 @@ export default Ember.Route.extend({
       cards: this.store.findAll('card')
     });
   },
+  currentGame: {sets_found:0, over:false},
   actions: {
     validSet () {
     },
-    createGame (game) {
-      let gameRecord = this.get('store').createRecord('game', game);
-      gameRecord.save();
-    }
+    createGame () {
+      let gameRecord = this.get('store').createRecord('game', this.get('currentGame'));
+      let self = this
+      gameRecord.save()
+      .then(function(savedgame) {
+        self.get('currentGame').id = savedgame.get('id')
+      });
+    },
+    deleteGame () {
+      this.get('store').findRecord('game', this.get('currentGame').id).then(function(game) {
+        game.destroyRecord()
+      });
+      debugger
+      // gameRecord.destroyRecord()
+
+    },
+    updateGame () {
+    },
   }
 });
