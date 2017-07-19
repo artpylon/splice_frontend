2462fe5ea485861e8f04a4bfc12df137d06560b1
artpylon <matthewgray88@gmail.com>
2017-07-19 09:49:32 -0400

    Update game when valid set is found Added updateGame action to game-board Updated toggleSelect action with a local vairable to increment number of sets found in the game and a setter to update the current game object, then used .send method to call the update action in the game-board.js. The update game action then is passed up to the /game route and the current game record is saved.

--- app/components/game-board.js
+++ app/components/game-board.js
@@ -60,28 +60,37 @@ export default Ember.Component.extend({
 
   actions: {
     toggleSelect (card) {
-      debugger
+      let sets = 0
+      let self = this
       if (this.get('selectedArray').length < 3) {
         this.get('selectedArray').push(card)
 
         if (this.get('selectedArray').length === 3 &&
           this.get('validate') === true) {
+            // record that a set was found on this game
+            sets++
+            self.get('game').set('sets_found', sets)
+            this.send('updateGame')
+            // remove valid set from game array
             this.get('gameArray').removeObjects(this.get('selectedArray'))
-
+            // add 3 new cards to the game away from the deck
             this.get('gameArray').addObject(this.get('deck').shiftObject())
             this.get('gameArray').addObject(this.get('deck').shiftObject())
             this.get('gameArray').addObject(this.get('deck').shiftObject())
-
+            // clear the selected array
             this.get('selectedArray').removeAt(0, 3)
 
-            return this.sendAction('validSet', this.get('selectedArray'));
         } else return
         // display error message
       } else return
         // display error message
+      // return this.sendAction('updateGame', self.get('game'));
     },
     deleteGame () {
       return this.sendAction('deleteGame', this.get('game'));
+    },
+    updateGame: function () {
+      return this.sendAction('updateGame', this.get('game'))
     },
   },
   // removeGameArray: Ember.computed('this.deck', function () {
