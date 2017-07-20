855c3de3f8b0cc89b9569679dbec834ff7b722bc
artpylon <matthewgray88@gmail.com>
2017-07-20 10:08:37 -0400

    Move play game button to index, add how to play content How to play content on index and /games Created component for play-game button Create service file for play-game button functionality Next, Move play game button to component

--- app/components/play-game.js
+++ app/components/play-game.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+export default Ember.Component.extend({
   auth: Ember.inject.service(),
 
   user: Ember.computed.alias('auth.credentials.email'),
   isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
+  actions: {
+    createGame: function  () {
+      return this.sendAction('createGame')
+    }
+  }
 });
