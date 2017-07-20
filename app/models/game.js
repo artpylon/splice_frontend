2e79b7af97a9c99f74559b6597a11e14f76d7e08
artpylon <matthewgray88@gmail.com>
2017-07-20 09:19:45 -0400

    Define one-many relationship in user and game models Add editable property to game model

--- app/models/game.js
+++ app/models/game.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  user: DS.belongsTo('user'),
   sets_found: DS.attr('number'),
-  over: DS.attr('boolean')
+  over: DS.attr('boolean'),
+  editable: DS.attr('boolean')
 });
