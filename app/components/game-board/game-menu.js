0eacc3a498f20327db3f45b4afb6f213d6cc93d5
artpylon <matthewgray88@gmail.com>
2017-07-25 16:53:34 -0400

    Created game-board/game-menu component Moved game menu code into a new component Sent required data down, and actions up (addThree, deleteGame and playAgain)

---
+++ app/components/game-board/game-menu.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    deleteGame () {
+      return this.sendAction('deleteGame', this.get('game'));
+    },
+    updateGame: function () {
+      return this.sendAction('updateGame', this.get('game'))
+    },
+    playAgain: function () {
+      return this.sendAction('playAgain')
+    },
+    addThree: function () {
+      return this.sendAction('addThree')
+    }
+  }
+});
